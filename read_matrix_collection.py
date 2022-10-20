95d54038c507471928a494d6db6111ced5c368ba
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 16:33:24 +0000

    Adding tarfile member sanitization to extractall()

--- read_matrix_collection.py
+++ read_matrix_collection.py
@@ -36,7 +36,26 @@ def read_data():
     for root, dirs, files in os.walk("data/data_zipped"):
         for file in files:
             with tarfile.open(f'data/data_zipped/{file}', mode='r') as f:
-                f.extractall(path='data/data_unzipped')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, path="data/data_unzipped")
 
     for root, dirs, _ in os.walk("data/data_unzipped"):
         for dir in dirs:
